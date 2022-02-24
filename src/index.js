9e6bef9217ee04a142df5f10ca2186b45c4510d1
mai <Mai9550>
2022-02-24 19:14:25 +0200

    5th

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+// Library Code
+function createStore (reducer) {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state.
+  // 3. Listen to changes on the state.
+  // 4. Update the state
+
+  let state
+  let listeners = []
+
+  const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  }
+}
+
+// App Code
 function todos (state = [], action) {
   switch(action.type) {
     case 'ADD_TODO' :
@@ -11,50 +44,85 @@ function todos (state = [], action) {
       return state
   }
 }
-function createStore (reducer) {
 
-  let state
-  let listeners=[]
-
-  const getState = () => state
-
-  const subscribe = (listener)=>{
-    listeners.push(listener)
-    return () =>{
-      listeners = listeners.filter((l)=>l!==listener)
-    }
+function goals (state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
   }
-
-  const dispatch = (action) =>{
-    state = todos(state,action)
-    listeners.forEach((listener)=>listener())
-  }
-
-
-  return {
-    getState,
-    subscribe,
-    dispatch
-  }
-  
 }
 
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
 
+const store = createStore(app)
 
-
-const store = createStore(todos)
-
-store.subscribe(()=>{console.log('The new state is:',store.getState())})
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
 
 store.dispatch({
   type: 'ADD_TODO',
   todo: {
     id: 0,
-    name: 'Learn Redux',
-    complete: false
+    name: 'Walk the dog',
+    complete: false,
   }
 })
 
-const unsubscribe=store.subscribe(()=>{console.log('The store changed')})
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Wash the car',
+    complete: false,
+  }
+})
 
-unsubscribe()
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 2,
+    name: 'Go to the gym',
+    complete: true,
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  id: 1
+})
+
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Learn Redux'
+  }
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 1,
+    name: 'Lose 20 pounds'
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_GOAL',
+  id: 0
+})
